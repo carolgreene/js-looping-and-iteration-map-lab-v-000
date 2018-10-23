3055d30674dc7c05e517c43b241efc3007f3014b
Carol Greene <carolgreene3@gmail.com>
2018-10-23 00:04:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function nameToAttributes(drivers) {
 }
 
 function attributesToPhrase(drivers) {
-  return drivers.map(function(driver)
-return `${driver.name} is from ${driver.hometown}`)
+  return drivers.map(function(driver) {
+    return `${driver.name} is from ${driver.hometown}`
+  }
 }
